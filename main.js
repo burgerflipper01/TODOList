4b482464d5afe444a1cc2eb4b119bacc7f7755e4
AndersMikkelsen <ande077h@gmail.com>
2023-09-18 08:37:58 +0200

    Not finished

--- main.js
+++ main.js
@@ -3,5 +3,26 @@ window.addEventListener('load',  () => {
     const input = document.querySelector("#new-task-input");
     const list_element = document.querySelector("#tasks");
 
-    console.log(form);
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        
+        const task = input.value;
+        
+        if (!task){
+            alert("Please enter a task");
+            return;
+        }
+
+        const task_el = document.createElement("div");
+        task_el.classList.add("task");
+
+        const task_content_el = document.createElement("div");
+        task_content_el.classList.add("content");
+        task_content_el.innerText = task;
+
+        task_el.appendChild(task_content_el);
+
+        list_element.appendChild(task_el);
+        
+
 });
